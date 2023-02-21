3593fcaebf033ea5aeff107ef3a727bdc64e94aa
cconzen <104637624+cconzen@users.noreply.github.com>
2023-02-22 00:11:26 +0100

    The Sun crawler

--- crawler/sun.py
+++ crawler/sun.py
@@ -2,31 +2,43 @@ import scrapy
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from scrapy.selector import Selector
+import os
+import io
 
 
 class SunSpider(scrapy.Spider):
     name = "sun"
-
     # using only one query to start with
     start_urls = ["https://www.thesun.co.uk/?s=qatar+world+cup"]
+    # all result pages ( = the last year; range 2 to 223 bc page one is differently named ^)
+    for i in range(2, 223):
+        start_urls.append(f"https://www.thesun.co.uk/page/{i}/?s=qatar+world+cup%2F")
+
+    try:
+        os.remove("sun_hreflist.txt")
+    except OSError:
+        pass
 
     def __init__(self, **kwargs):
+        """initialise selenium webdriver"""
         super().__init__(**kwargs)
         chrome_options = Options()
         chrome_options.add_argument("--headless")  # comment line to make browser visible
         self.driver = webdriver.Chrome(options=chrome_options)
 
     def parse(self, response, **kwargs):
+        """parses raw response to get all href links, also writes a txt file containing them."""
         self.driver.get(response.url)
         sel = Selector(text=self.driver.page_source)
         article_links = sel.css("a.teaser-anchor--search::attr(href)").getall()
+
         for link in article_links:
-            with open("sun_hreflist.txt", 'a') as file:
-                file.truncate()
+            with io.open("sun_hreflist.txt", "a", encoding="utf8") as file:
                 file.write(link + "\n")
             yield scrapy.Request(link, callback=self.parse_article)
 
     def parse_article(self, response):
+        """parses an article for metadata and yields a dictionary containing them as key value pairs."""
         title = response.css("h1.article__headline::text").get()
         author = response.css("a.article__author-link::text").get()
         date = response.css("span.article__timestamp::text").get()
@@ -41,4 +53,5 @@ class SunSpider(scrapy.Spider):
         }
 
     def closed(self, reason):
+        """closes webdriver"""
         self.driver.quit()
